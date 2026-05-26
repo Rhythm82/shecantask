b4d6f4db9508710bca1636637cf5267fb289624a
Rhythm Das <dasrhythm2@gmail.com>
2026-05-27 01:45:29 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ app.use(
   cors({
     origin: (origin, callback) => {
       const allowedOrigins = [
-        process.env.CLIENT_URL,
+        "https://shecan-connect.onrender.com",
         "http://localhost:5173",
         "http://127.0.0.1:5173"
       ].filter(Boolean);
